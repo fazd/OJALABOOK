90e9e4107b3bccc5d346aa4b18a483c81f25de47
Fabio A. Zapata D <fabioandres2907@hotmail.com>
2015-12-17 18:21:04 -0500

    creado y agregardo estatus

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OJALABOOK::Application.config.secret_token = '48166547a5a9b103fdf2cf9c74f587a05342cf4b405b52e80a4f40ba763d07358d8501f39facc60cee593ca604551bc72d937103c3eff5950f7cb1609f634350'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+OJALABOOK::Application.config.session_store :cookie_store, key: '_OJALABOOK_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# OJALABOOK::Application.config.session_store :active_record_store
