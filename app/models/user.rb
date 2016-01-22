1bf863f513d6b92ae4d6e778de1fec89a5e910e2
Fabio A. Zapata D <fabioandres2907@hotmail.com>
2016-01-22 08:10:42 -0500

    meti usuario

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :nombre, :apellido, :nombre_de_usuario, :remember_me, :email, :encrypted_password
+  # attr_accessible :title, :body
+end
